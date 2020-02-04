5ffbe251f27a028a26da1c778e0acca525dea640
labis7 <labis_skoufis@hotmail.com>
2020-02-05 00:48:07 +0200

    Tesla :) V0.4

---
+++ highway_driving.py
@@ -0,0 +1,95 @@
+"""Sample Webots controller for highway driving benchmark."""
+
+from vehicle import Driver
+
+# name of the available distance sensors
+sensorsNames = [
+    'front',
+    'front right 0',
+    'front right 1',
+    'front right 2',
+    'front left 0',
+    'front left 1',
+    'front left 2',
+    'rear',
+    'rear left',
+    'rear right',
+    'right',
+    'left']
+sensors = {}
+
+maxSpeed = 82
+driver = Driver()
+driver.setSteeringAngle(0.0)  # go straight
+
+
+
+# get and enable the distance sensors
+for name in sensorsNames:
+    sensors[name] = driver.getDistanceSensor('distance sensor ' + name)
+    sensors[name].enable(10)
+
+# get and enable the GPS
+gps = driver.getGPS('gps')
+gps.enable(10)
+
+# get the camera
+camera = driver.getCamera('camera')
+# uncomment those lines to enable the camera
+#camera.enable(50)
+#camera.recognitionEnable(50)
+dest =0
+prevpos = -2
+while driver.step() != -1:
+    # adjust the speed according to the value returned by the front distance sensor
+    frontDistance = sensors['front'].getValue()
+    frontRange = sensors['front'].getMaxValue()
+    speed = maxSpeed * frontDistance / frontRange
+    driver.setCruisingSpeed(speed)
+    # brake if we need to reduce the speed
+    speedDiff = driver.getCurrentSpeed() - speed
+    x=gps.getValues()
+    
+    if(dest == 2):#1.3
+        #if(x[0]<1.3): #then turn left
+        driver.setSteeringAngle(60*(x[0] - prevpos)*0.05 + (x[0]-1.3)*0.02) #TUNNING!!
+        #if(x[0]>1.3): #then turn right
+        #    driver.setSteeringAngle(1*0.1)
+
+        #continue
+    if(dest == 1):# 5.2
+       #if(x[0]<1.3): #then turn left
+       driver.setSteeringAngle(60*(x[0] - prevpos)*0.05 + (x[0]-5.2)*0.02)
+       #if(x[0]>1.3): #then turn right
+       #    driver.setSteeringAngle(1*0.1)
+ 
+       #continue   
+    if(dest == 3):# -2
+       #if(x[0]<1.3): #then turn left
+       driver.setSteeringAngle(60*(x[0] - prevpos)*0.05 + (x[0]+2)*0.02)
+       #if(x[0]>1.3): #then turn right
+       #    driver.setSteeringAngle(1*0.1)
+       prevpos = x[0]
+       #continue    
+    prevpos = x[0] 
+    if speedDiff > 0:
+        if((sensors['left'].getValue()>3)and(sensors['front left 2'].getValue()>8)): #You can steer left  
+            #driver.setSteeringAngle(-speedDiff*0.1)
+            if((x[0]<-1.5)and(x[0]>-2.5)): #-2, you are in the right row, go left
+                dest = 2 #middle
+            if((x[0]<1.5)and(x[0]>1)):  #1.3, you are middle,go left
+                dest = 1 #left
+        elif((sensors['right'].getValue()>3)and(sensors['front right 2'].getValue()>8)): #you can turn right
+            if((x[0]<5.25)and(x[0]>5.15)): #means you are left, go middle
+                dest = 2 #middle
+            if((x[0]<1.5)and(x[0]>1)):  #1.3, you are middle,go right
+                dest = 3 #right
+        else:    
+            driver.setBrakeIntensity(min(speedDiff / speed, 1))
+    else:
+        driver.setBrakeIntensity(0)
+        
+    
+
+        
+        
